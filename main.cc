cbb2a791ba06ed3a20cc38295b0680831e57a166
josephreisinger <joe@premise.com>
2014-08-14 15:13:10 -0700

    load images more often; dont scroll

--- main.cc
+++ main.cc
@@ -19,9 +19,11 @@ public:
 
   // Run() implementation needs to check running_ regularly.
 
+
 protected:
   volatile bool running_;  // TODO: use mutex, but this is good enough for now.
   RGBMatrix *const matrix_;
+
 };
 
 // Pump pixels to screen. Needs to be high priority real-time because jitter
@@ -39,136 +41,25 @@ public:
 
 // -- The following are demo image generators.
 
-// Simple generator that pulses through RGB and White.
-class ColorPulseGenerator : public RGBMatrixManipulator {
-public:
-  ColorPulseGenerator(RGBMatrix *m) : RGBMatrixManipulator(m) {}
-  void Run() {
-    const int width = matrix_->width();
-    const int height = matrix_->height();
-    uint32_t count = 0;
-    while (running_) {
-      usleep(5000);
-      ++count;
-      int color = (count >> 9) % 6;
-      int value = count & 0xFF;
-      if (count & 0x100) value = 255 - value;
-      int r, g, b;
-      switch (color) {
-      case 0: r = value; g = b = 0; break;
-      case 1: r = g = value; b = 0; break;
-      case 2: g = value; r = b = 0; break;
-      case 3: g = b = value; r = 0; break;
-      case 4: b = value; r = g = 0; break;
-      default: r = g = b = value; break;
-      }
-      for (int x = 0; x < width; ++x)
-        for (int y = 0; y < height; ++y)
-          matrix_->SetPixel(x, y, r, g, b);
-    }
-  }
-};
-
-class SimpleSquare : public RGBMatrixManipulator {
-public:
-  SimpleSquare(RGBMatrix *m) : RGBMatrixManipulator(m) {}
-  void Run() {
-    const int width = matrix_->width();
-    const int height = matrix_->height();
-    // Diagonaly
-    for (int x = 0; x < width; ++x) {
-        matrix_->SetPixel(x, x, 255, 255, 255);
-        matrix_->SetPixel(height -1 - x, x, 255, 0, 255);
-    }
-    for (int x = 0; x < width; ++x) {
-      matrix_->SetPixel(x, 0, 255, 0, 0);
-      matrix_->SetPixel(x, height - 1, 255, 255, 0);
-    }
-    for (int y = 0; y < height; ++y) {
-      matrix_->SetPixel(0, y, 0, 0, 255);
-      matrix_->SetPixel(width - 1, y, 0, 255, 0);
-    }
-  }
-};
-
-// Simple class that generates a rotating block on the screen.
-class RotatingBlockGenerator : public RGBMatrixManipulator {
-public:
-  RotatingBlockGenerator(RGBMatrix *m) : RGBMatrixManipulator(m) {}
-
-  uint8_t scale_col(int val, int lo, int hi) {
-    if (val < lo) return 0;
-    if (val > hi) return 255;
-    return 255 * (val - lo) / (hi - lo);
-  }
-
-  void Run() {
-    const int cent_x = matrix_->width() / 2;
-    const int cent_y = matrix_->height() / 2;
-
-    // The square to rotate (inner square + black frame) needs to cover the
-    // whole area, even if diagnoal.
-    const int rotate_square = min(matrix_->width(), matrix_->height()) * 1.41;
-    const int min_rotate = cent_x - rotate_square / 2;
-    const int max_rotate = cent_x + rotate_square / 2;
-
-    // The square to display is within the visible area.
-    const int display_square = min(matrix_->width(), matrix_->height()) * 0.7;
-    const int min_display = cent_x - display_square / 2;
-    const int max_display = cent_x + display_square / 2;
-
-    const float deg_to_rad = 2 * 3.14159265 / 360;
-    int rotation = 0;
-    while (running_) {
-      ++rotation;
-      usleep(15 * 1000);
-      rotation %= 360;
-      for (int x = min_rotate; x < max_rotate; ++x) {
-        for (int y = min_rotate; y < max_rotate; ++y) {
-          float disp_x, disp_y;
-          Rotate(x - cent_x, y - cent_y,
-                 deg_to_rad * rotation, &disp_x, &disp_y);
-          if (x >= min_display && x < max_display &&
-              y >= min_display && y < max_display) { // within display square
-            matrix_->SetPixel(disp_x + cent_x, disp_y + cent_y,
-                              scale_col(x, min_display, max_display),
-                              255 - scale_col(y, min_display, max_display),
-                              scale_col(y, min_display, max_display));
-          } else {
-            // black frame.
-            matrix_->SetPixel(disp_x + cent_x, disp_y + cent_y, 0, 0, 0);
-          }
-        }
-      }
-    }
-  }
-
-private:
-  void Rotate(int x, int y, float angle,
-              float *new_x, float *new_y) {
-    *new_x = x * cosf(angle) - y * sinf(angle);
-    *new_y = x * sinf(angle) + y * cosf(angle);
-  }
-};
 
 class ImageScroller : public RGBMatrixManipulator {
 public:
-  ImageScroller(RGBMatrix *m)
-    : RGBMatrixManipulator(m), image_(NULL), horizontal_position_(0) {
+  ImageScroller(RGBMatrix *m, const char *filename)
+    : RGBMatrixManipulator(m), image_(NULL), horizontal_position_(0), filename_(filename) {
   }
 
   // _very_ simplified. Can only read binary P6 PPM. Expects newlines in headers
   // Not really robust. Use at your own risk :)
-  bool LoadPPM(const char *filename) {
+  bool LoadPPM() {
     if (image_) {
       delete [] image_;
       image_ = NULL;
     }
-    FILE *f = fopen(filename, "r");
+    FILE *f = fopen(filename_, "r");
     if (f == NULL) return false;
     char header_buf[256];
     const char *line = ReadLine(f, header_buf, sizeof(header_buf));
-#define EXIT_WITH_MSG(m) { fprintf(stderr, "%s: %s |%s", filename, m, line); \
+#define EXIT_WITH_MSG(m) { fprintf(stderr, "%s: %s |%s", filename_, m, line); \
       fclose(f); return false; }
     if (sscanf(line, "P6 ") == EOF)
       EXIT_WITH_MSG("Can only handle P6 as PPM type.");
@@ -197,6 +88,9 @@ public:
     const int screen_height = matrix_->height();
     const int screen_width = matrix_->width();
     while (running_) {
+      if (!LoadPPM())
+          return;
+
       if (image_ == NULL) {
         usleep(100 * 1000);
         continue;
@@ -211,10 +105,9 @@ public:
           matrix_->SetPixel(disp_x, disp_y, p.red, p.green, p.blue);
         }
       }
-      ++horizontal_position_;
+      // ++horizontal_position_;
     }
   }
-
 private:
   struct Pixel {
     uint8_t red;
@@ -241,6 +134,9 @@ private:
   int height_;
   Pixel *image_;
   uint32_t horizontal_position_;
+
+  const char* filename_;
+
 };
 
 int main(int argc, char *argv[]) {
@@ -257,30 +153,12 @@ int main(int argc, char *argv[]) {
   RGBMatrix m(&io);
     
   RGBMatrixManipulator *image_gen = NULL;
-  switch (demo) {
-  case 0:
-    image_gen = new RotatingBlockGenerator(&m);
-    break;
-
-  case 1:
-    if (argc > 2) {
-      ImageScroller *scroller = new ImageScroller(&m);
-      if (!scroller->LoadPPM(argv[2]))
-        return 1;
+  if (argc > 2) {
+      ImageScroller *scroller = new ImageScroller(&m, argv[2]);
       image_gen = scroller;
-    } else {
+  } else {
       fprintf(stderr, "Demo %d Requires PPM image as parameter", demo);
       return 1;
-    }
-    break;
-
-  case 2:
-    image_gen = new SimpleSquare(&m);
-    break;
-
-  default:
-    image_gen = new ColorPulseGenerator(&m);
-    break;
   }
 
   if (image_gen == NULL)
